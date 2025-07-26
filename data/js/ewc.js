37b19e228d8e62eae853a98000ced09cf7507754
Albert <80.albert@gmail.com>
2025-07-26 12:27:25 +0800

    u

--- data/js/ewc.js
+++ data/js/ewc.js
@@ -9,8 +9,8 @@
   iframe.style.position = 'fixed';
   iframe.style.bottom = '20px';
   iframe.style.right = '20px';
-  iframe.style.width = '360px';
-  iframe.style.height = '540px';
+  iframe.style.width = '480px';
+  iframe.style.height = '660px';
   iframe.style.border = 'none';
   iframe.style.borderRadius = '12px';
   iframe.style.zIndex = '9999';
